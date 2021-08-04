58aa8ef343e73efb4d14b4e6114d6cac4ab3389e
novi_indra_f <novi.indra1453@gmail.com>
2021-08-04 18:35:31 +0700

    Implement Design UI Firebase

---
+++ app/src/main/java/com/nifcompany/firebaseauthcoroutine/ui/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.nifcompany.firebaseauthcoroutine.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nifcompany.firebaseauthcoroutine.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
